36f550d84ce7db3bb4cad47930bb4d6353eec9ee
Mads Opstad Reistadbakk <mareis@Madss-MBP.byod.mrfylke.no>
2019-09-19 13:38:39 +0200

    ,

--- 02_Oppgaver/02_Innleveringer/.ipynb_checkpoints/ord-checkpoint.py
+++ 02_Oppgaver/02_Innleveringer/.ipynb_checkpoints/ord-checkpoint.py
@@ -1,6 +1,6 @@
 import random
 
-liste_ord = ['Bredbånd', 'Bredformat', 'Byte', 'Bit', 'Grensesnitt', 'Internett', 'Komprimering', 'Kryptering', 'Innrykkw']
+liste_ord = ['Bredbånd', 'Bredformat', 'Byte', 'Bit', 'Grensesnitt', 'Internett', 'Komprimering', 'Kryptering', 'Innrykk']
 def lag_ord():
     tilfeldig = random.randint(0, len(liste_ord) - 1)
     return liste_ord[tilfeldig]
