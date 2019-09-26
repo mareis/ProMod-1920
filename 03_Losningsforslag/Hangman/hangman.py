60a9bdf57ff87eb26cb28d64c4fa0780b3da0472
Mads Opstad Reistadbakk <mareis@Madss-MBP.byod.mrfylke.no>
2019-09-26 13:47:24 +0200

    ,

--- 03_Losningsforslag/Hangman/hangman.py
+++ 03_Losningsforslag/Hangman/hangman.py
@@ -1,3 +1,30 @@
 import ord
+import os
 
-ord.trekk_ord()
+
+tilgeldig_ord = ord.trekk_ord()
+losning = tilgeldig_ord[0]
+
+for i in range(len(tilgeldig_ord)-1):
+    losning += "_"
+
+
+def clr():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('\n \n')
+
+
+def print_ord():
+    clr()
+    for tegn in losning:
+        if tegn == "_":
+            print("___ ", end="")
+        else:
+            print(tegn+" ", end="")
+    print('')
+
+
+def
+
+
+print_ord()
